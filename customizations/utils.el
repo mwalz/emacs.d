c523c7b2b96b5c577b79ab3c2a577a40e3c9a8e3
Ilya Sterin <isterin@gmail.com>
2011-04-22 14:17:18 -0400

    Added auto-completions

--- customizations/utils.el
+++ customizations/utils.el
@@ -1,3 +1,9 @@
 ;; Enable textmate bindings/tools
 (require 'textmate)
 (textmate-mode)
+
+
+;; Autocomplete
+(add-to-list 'ac-dictionary-directories (concat dotfiles-dir "dict"))
+(require 'auto-complete-config)
+(ac-config-default)
